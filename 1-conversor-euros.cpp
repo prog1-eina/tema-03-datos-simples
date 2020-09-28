038f2ffb02d67e7603dde34defd35169e011ef43
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-09-28 11:30:52 +0200

    Constante PTAS_POR_EURO como constante global

--- 1-conversor-euros.cpp
+++ 1-conversor-euros.cpp
@@ -11,11 +11,16 @@
 using namespace std;
 
 /*
+ * Tasa de cambio oficial entre pesetas y euro
+ */
+const double PTAS_POR_EURO = 166.386;
+
+
+/*
  * Programa que escribe en la pantalla la cantidad que
  * equivale en euros a 2000 pesetas.
  */
 int main() {
-    const double PTAS_POR_EURO = 166.386;
     int pesetas = 2000;
     double euros = pesetas / PTAS_POR_EURO;
     cout << fixed << setprecision(2) << euros << " euros" << endl;
--- 2-conversor-euros-interactivo.cpp
+++ 2-conversor-euros-interactivo.cpp
@@ -11,16 +11,23 @@
 using namespace std;
 
 /*
+ * Tasa de cambio oficial entre pesetas y euro
+ */
+const double PTAS_POR_EURO = 166.386;
+
+
+/*
  * Programa que escribe en la pantalla la cantidad
  * equivalente en euros a una cantidad de dinero entera
  * expresada en pesetas solicitada previamente al usuario.
  */
 int main() {
-    const double PTAS_POR_EURO = 166.386;
     cout << "Escriba una cantidad en pesetas: ";
     int pesetas;
     cin >> pesetas;
+
     double euros = pesetas / PTAS_POR_EURO;
+    
     cout << fixed << setprecision(2) << euros << " euros" << endl;
     return 0;
 }
