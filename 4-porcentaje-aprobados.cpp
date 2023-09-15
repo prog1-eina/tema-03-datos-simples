c744848798cb01bf13f627f3a11439f7a4ef40de
Miguel Ángel Latre Abadía <latre@unizar.es>
2023-09-15 10:19:58 +0200

    4-porcentaje-aprobados.cpp: se recalca que es **erróneo**

--- 4-porcentaje-aprobados.cpp
+++ 4-porcentaje-aprobados.cpp
@@ -2,13 +2,13 @@
  * Curso de Programación 1. Tema 1
  * Autores: Miguel Ángel Latre
  * Última revisión: 22 de septiembre de 2022
- * Resumen: Programa erróneo que pretende escribir en la pantalla un porcentaje. 
+ * Resumen: Programa ERRÓNEO que pretende escribir en la pantalla un porcentaje. 
  ******************************************************************************/
 #include <iostream>
 using namespace std;
 
 /*
- * Programa erróneo que pretende escribir en la pantalla el porcentaje de
+ * Programa ERRÓNEO que pretende escribir en la pantalla el porcentaje de
  * aprobados correspondiente a 95 estudiantes aprobados con respecto a 160
  * estudiantes matriculados.
  */
